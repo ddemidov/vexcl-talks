0093043eed78bdad98603cc7e2deb6107eff257c
Denis Demidov <ddemidov@ksu.ru>
2013-08-16 22:11:08 +0400

    colorscheme

--- chameleon.m
+++ chameleon.m
@@ -1,12 +1,12 @@
 function rgb = chameleon(n)
     switch n
     case 1
-        rgb = [140,  64,  20] / 255;
+        rgb = [ 39,  45,  64] / 255;
     case 2
-        rgb = [217, 171, 130] / 255;
+        rgb = [157, 191, 142] / 255;
     case 3
-        rgb = [191, 143,  84] / 255;
+        rgb = [ 85, 115, 109] / 255;
     case 4
-        rgb = [166, 105,  51] / 255;
+        rgb = [ 54,  70,  89] / 200;
     end
 end
