84d2aa7ed65d07454c1231c7367fb6f61df09db9
Denis Demidov <ddemidov@ksu.ru>
2013-09-06 12:27:20 +0400

    Rerun naive example with updated vexcl

--- code/lorenz/run.sh
+++ code/lorenz/run.sh
@@ -4,11 +4,12 @@ export OCL_DEVICE=K20
 
 for run in vexcl_generated vexcl_naive thrust cublas; do
     echo ${run}
+    rm ${run}.dat
     for((n=256; n<=4194304; n*=2)); do
         echo "  $n"
         for((a=1; a<=10; a++)); do
             echo "    $a"
-            ./${run} >> ${run}.dat
+            ./${run} ${n} >> ${run}.dat
         done
     done
 done
