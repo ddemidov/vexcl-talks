e63faa88cb2b6077aa5fc2ff8847fe8e5974b727
Denis Demidov <dennis.demidov@gmail.com>
2015-10-14 16:03:23 +0300

    [wip] kazan

--- code/hello-opencl.cpp
+++ code/hello-opencl.cpp
@@ -1,70 +1,55 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stdexcept>
 
 #define __CL_ENABLE_EXCEPTIONS
 #include <CL/cl.hpp>
 
 int main() {
-    // Get list of OpenCL platforms.
     std::vector<cl::Platform> platform;
     cl::Platform::get(&platform);
 
     if (platform.empty())
         throw std::runtime_error("No OpenCL platforms");
 
-    // Get first available GPU.
     cl::Context context;
     std::vector<cl::Device> device;
     for(auto p = platform.begin(); device.empty() && p != platform.end(); p++) {
-        std::vector<cl::Device> pldev;
-        try {
-            p->getDevices(CL_DEVICE_TYPE_GPU, &pldev);
-            for(auto d = pldev.begin(); device.empty() && d != pldev.end(); d++) {
-                if (!d->getInfo<CL_DEVICE_AVAILABLE>()) continue;
-                device.push_back(*d);
+        std::vector<cl::Device> dev;
+        p->getDevices(CL_DEVICE_TYPE_GPU, &dev);
+        for(auto d = dev.begin(); device.empty() && d != dev.end(); d++) {
+            if (!d->getInfo<CL_DEVICE_AVAILABLE>()) continue;
+            device.push_back(*d);
+            try {
                 context = cl::Context(device);
+            } catch(...) {
+                device.clear();
             }
-        } catch(...) {
-            device.clear();
         }
     }
     if (device.empty()) throw std::runtime_error("No GPUs");
 
-    // Create command queue.
     cl::CommandQueue queue(context, device[0]);
 
-    // Prepare input data, transfer it to the device.
-    const size_t N = 1 << 20;
-    std::vector<float> a(N, 1), b(N, 2), c(N);
+    const size_t n = 1024 * 1024;
+    std::vector<double> a(n, 1.5), b(n, 2.7);
 
     cl::Buffer A(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-            a.size() * sizeof(float), a.data());
+            a.size() * sizeof(a[0]), a.data());
 
-    cl::Buffer B(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-            b.size() * sizeof(float), b.data());
+    cl::Buffer B(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
+            b.size() * sizeof(b[0]), b.data());
 
-    cl::Buffer C(context, CL_MEM_READ_WRITE,
-            c.size() * sizeof(float));
-
-    // Create kernel source
-    const char source[] = R"(
-        kernel void add(
-          ulong n,
-          global const float *a,
-          global const float *b,
-          global float *c
-          )
-        {
+    std::string source = R"(
+        kernel void add(ulong n, global const double *a, global double *b) {
           ulong i = get_global_id(0);
-          if (i < n) c[i] = a[i] + b[i];
+          if (i < n) b[i] += a[i];
         }
         )";
 
-    // Compile OpenCL program for the device.
-    cl::Program program(context, cl::Program::Sources(
-                1, std::make_pair(source, strlen(source))
-                ));
+    cl::Program program(context, source);
+
     try {
         program.build(device);
     } catch (const cl::Error&) {
@@ -76,16 +61,12 @@ int main() {
     }
     cl::Kernel add(program, "add");
 
-    // Set kernel parameters.
-    add.setArg(0, static_cast<cl_ulong>(N));
+    add.setArg(0, static_cast<cl_ulong>(n));
     add.setArg(1, A);
     add.setArg(2, B);
-    add.setArg(3, C);
 
-    // Launch kernel on the compute device.
-    queue.enqueueNDRangeKernel(add, cl::NullRange, N, cl::NullRange);
+    queue.enqueueNDRangeKernel(add, cl::NullRange, n, cl::NullRange);
 
-    // Get result back to host.
-    queue.enqueueReadBuffer(C, CL_TRUE, 0, c.size() * sizeof(float), c.data());
-    std::cout << c[42] << std::endl; // Should get '3' here.
+    queue.enqueueReadBuffer(B, CL_TRUE, 0, b.size() * sizeof(b[0]), b.data());
+    std::cout << b[42] << std::endl;
 }
