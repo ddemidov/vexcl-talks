640bd7807b35eb504dfe64d629c030c8bb9dbaf0
Denis Demidov <dennis.demidov@gmail.com>
2016-05-18 20:24:14 +0300

    expression templates

---
+++ mcpi.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import sys
+import numpy as np
+from chameleon import *
+
+if (len(sys.argv[1:]) > 0):
+    import matplotlib
+    matplotlib.use('Agg')
+    outfile = sys.argv[1]
+    quiet = True
+else:
+    quiet = False
+
+from pylab import *
+
+rc('font',   size=22)
+figure(figsize=(7,7))
+
+x = np.random.uniform(0, 1, 3000)
+y = np.random.uniform(0, 1, 3000)
+
+i = (x * x + y * y) < 1.0
+
+plot(x[i==1], y[i==1], 'o', markersize=3, markeredgecolor=chameleon1, markerfacecolor=chameleon1)
+plot(x[i==0], y[i==0], 'o', markersize=3, markeredgecolor=chameleon2, markerfacecolor=chameleon2)
+
+x = np.linspace(0, 1, 100)
+y = np.sqrt(1 - x * x)
+
+plot(x, y, '-', color='k', linewidth=1)
+
+xticks([0, 0.5, 1])
+yticks([   0.5, 1])
+tight_layout()
+show()
+
+if quiet: savefig(outfile)
+else:     show()
+
